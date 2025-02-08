cf8cfd8e2b23054afb6af72feea1953e252feb4a
abdallahsalimjuma <abdallahsalimjuma35@gmail.com>
2025-02-08 20:02:31 +0300

    Update fpp.js

--- commandes/fpp.js
+++ commandes/fpp.js
@@ -1,5 +1,5 @@
 const { zokou } = require("../framework/zokou");
-const { generateProfilePicture } = require("../keizzah/dl/Function");
+const { generateProfilePicture } = require("../framework/dl/Function");
 const { S_WHATSAPP_NET } = require('@whiskeysockets/baileys');
 const fs = require("fs");
 
