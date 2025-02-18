9efa7af51a0b50308c9c1c03ada253d9a550fed3
abdallahsalimjuma <abdallahsalimjuma35@gmail.com>
2025-02-18 07:43:30 +0300

    Update getall.js

--- commandes/getall.js
+++ commandes/getall.js
@@ -8,6 +8,13 @@ const { search, download } = require("aptoide-scraper");
 const fs = require("fs-extra");
 const conf = require("../set");
 const { default: axios } = require('axios');
+
+
+
+
+
+
+
 //const { uploadImageToImgur } = require('../framework/zokou');
     nomCom: "getall",
     categorie: "owner",
