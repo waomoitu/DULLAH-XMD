457287d45b837a8bfba1cdeb055e47f044ddcb43
abdallahsalimjuma <abdallahsalimjuma35@gmail.com>
2025-02-19 07:43:46 +0300

    Create dog.js

---
+++ commandes/dog.js
@@ -0,0 +1,20 @@
+const {
+   zokou
+} = require('../framework/zokou')
+zokou({
+   nomCom: "dog",
+   reaction: "🤔",
+  categories: "misc",
+   desc: "Send videos of randome dogs!
+   filename: __filename,
+},
+   async (dest, zk, commandeOptions) => {
+      try {
+         const fetch = require("node-fetch");
+         let res = await fetch('https://random.dog/woof.json')
+         let json = await res.json()
+         if (json.status) return await m.reply("*Request Denied!*")
+         m.bot.sendFileUrl(m.jid, json.url, "", m, { author: "Asta-Md" }, "video");
+
+      } catch (e) { m.error(`${e}\n\nCommand: dog`, e, false) }
+   });
