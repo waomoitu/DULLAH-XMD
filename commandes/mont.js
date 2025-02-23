8d2559ffc81b2c5470e018004a6fd32d9862cb7e
abdallahsalimjuma <abdallahsalimjuma35@gmail.com>
2025-02-23 11:03:16 +0300

    Update mont.js

--- commandes/mont.js
+++ commandes/mont.js
@@ -8,7 +8,7 @@ const s = require(__dirname + "/../set");
 const more = String.fromCharCode(8206)
 const Taphere = more.repeat(4001)
 
-ezra({ nomCom: "quran-menu", categorie: "Menu" }, async (dest, zk, commandeOptions) => {
+zokou({ nomCom: "quran-menu", categorie: "Menu" }, async (dest, zk, commandeOptions) => {
     let { ms, repondre ,prefixe,nomAuteurMessage,mybotpic} = commandeOptions;
     let { cm } = require(__dirname + "/../framework/zokou");
     var coms = {};
