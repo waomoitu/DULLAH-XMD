edf0dbde8eedb77f298f7d968650b4a4c62a3087
abdallahsalimjuma <abdallahsalimjuma35@gmail.com>
2025-03-05 18:40:20 +0300

    Create repor.js

---
+++ commandes/repor.js
@@ -0,0 +1,51 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const { zokou } = require("../framework/zokou");
+
+zokou({ nomCom: "repo4", catégorie: "General", reaction: "✨", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
+  const githubRepo = 'https://api.github.com/repos/abdallahsalimjuma/Dullah-xmd';
+  const img = 'https://files.catbox.moe/0cxusf.jpg';
+  const audioUrl = 'https://files.catbox.moe/533oqh.jpg'; // Ensure this is a valid audio URL
+
+  try {
+    const response = await fetch(githubRepo);
+    const data = await response.json();
+
+    if (data) {
+      const repoInfo = {
+        stars: data.stargazers_count,
+        forks: data.forks_count,
+        lastUpdate: data.updated_at,
+        owner: data.owner.login,
+      };
+
+      const releaseDate = new Date(data.created_at).toLocaleDateString('en-GB');
+      const lastUpdateDate = new Date(data.updated_at).toLocaleDateString('en-GB');
+
+      const gitdata = `*Hello Friend,
+this is* *ᴅᴜʟʟᴀʜ-xᴍᴅ v² 👊.*\n *Join Group Chat* https://chat.whatsapp.com/IdRXU9UcO8K50GPelOyhxh
+
+🗼 *REPOSITORY:* ${data.html_url}
+💫 *STARS:* ${repoInfo.stars}
+🧧 *FORKS:* ${repoInfo.forks}
+📅 *RELEASE DATE:* ${releaseDate}
+🕐 *LAST UPDATE:* ${repoInfo.lastUpdate}
+🙊 *OWNER:* *Mr. Dullah*
+🍃 *THEME:* *ᴅᴜʟʟᴀʜ-xᴍᴅ v²*
+🍷 *I Am Safe To Fight In My Life*
+__________________________________
+            *Made With ᴅᴜʟʟᴀʜ-xᴍᴅ v²*`;
+
+      // Send image with the repository details
+      await zk.sendMessage(dest, { image: { url: img }, caption: gitdata });
+
+      // Send audio file
+      await zk.sendMessage(dest, { audio: { url: audioUrl }, mimetype: 'audio/mp4' });
+
+    } else {
+      console.log("Could not fetch data");
+    }
+  } catch (error) {
+    console.log("Error fetching data:", error);
+  }
+});
