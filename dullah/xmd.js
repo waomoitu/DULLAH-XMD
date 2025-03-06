83b5a77cdbd177ff1892dfa6828b487182be016f
dullar255 <dullahbest04@gmail.com>
2025-03-06 23:34:14 +0300

    Update xmd.js

--- dullah/xmd.js
+++ dullah/xmd.js
@@ -1,1 +1,347 @@
+"use strict";
 
+// All fetched commands combined here
+
+// --- index.js ---
+require("./index");
+
+// --- AI.js ---
+require("./AI");
+
+// --- AI2.js ---
+require("./AI2");
+
+// --- GPT.js ---
+require("./GPT");
+
+// --- General.js ---
+require("./General");
+
+// --- GroupQuotes.js ---
+require("./GroupQuotes");
+
+// --- Lt.js ---
+require("./Lt");
+
+// --- Media_dl.js ---
+require("./Media_dl");
+
+// --- Mods.js ---
+require("./Mods");
+
+// --- Nokia.js ---
+require("./Nokia");
+
+// --- accapitalgm.js ---
+require("./accapitalgm");
+
+// --- afk.js ---
+require("./afk");
+
+// --- ahack.js ---
+require("./ahack");
+
+// --- alive.js ---
+require("./alive");
+
+// --- anime.js ---
+require("./anime");
+
+// --- ans.js ---
+require("./ans");
+
+// --- anti-delete.js ---
+require("./anti-delete");
+
+// --- anti-spam.js ---
+require("./anti-spam");
+
+// --- anti-sticker.js ---
+require("./anti-sticker");
+
+// --- antifake.js ---
+require("./antifake");
+
+// --- apk.js ---
+require("./apk");
+
+// --- audioedit.js ---
+require("./audioedit");
+
+// --- bible.js ---
+require("./bible");
+
+// --- bine.js ---
+require("./bine");
+
+// --- blocklist.js ---
+require("./blocklist");
+
+// --- boom.js ---
+require("./boom");
+
+// --- bug.js ---
+require("./bug");
+
+// --- canvascord.js ---
+require("./canvascord");
+
+// --- cat.js ---
+require("./cat");
+
+// --- chanel.js ---
+require("./chanel");
+
+// --- chatbot.js ---
+require("./chatbot");
+
+// --- chatpt.js ---
+require("./chatpt");
+
+// --- codetest.js ---
+require("./codetest");
+
+// --- conversation.js ---
+require("./conversation");
+
+// --- cricket.js ---
+require("./cricket");
+
+// --- delnote.js ---
+require("./delnote");
+
+// --- deobfuscate.js ---
+require("./deobfuscate");
+
+// --- deploy.js ---
+require("./deploy");
+
+// --- design.js ---
+require("./design");
+
+// --- dog.js ---
+require("./dog");
+
+// --- dullah.js ---
+require("./dullah");
+
+// --- dullaht.js ---
+require("./dullaht");
+
+// --- elod.js ---
+require("./elod");
+
+// --- events.js ---
+require("./events");
+
+// --- events2.js ---
+require("./events2");
+
+// --- fancy.js ---
+require("./fancy");
+
+// --- forward.js ---
+require("./forward");
+
+// --- fpp.js ---
+require("./fpp");
+
+// --- frnews.js ---
+require("./frnews");
+
+// --- fresavecontact.js ---
+require("./fresavecontact");
+
+// --- friends.js ---
+require("./friends");
+
+// --- games.js ---
+require("./games");
+
+// --- getall.js ---
+require("./getall");
+
+// --- groupe.js ---
+require("./groupe");
+
+// --- grp-set.js ---
+require("./grp-set");
+
+// --- guy.js ---
+require("./guy");
+
+// --- helper.js ---
+require("./helper");
+
+// --- hentai.js ---
+require("./hentai");
+
+// --- hentai2.js ---
+require("./hentai2");
+
+// --- humidity.js ---
+require("./humidity");
+
+// --- igdl-fb-tk.js ---
+require("./igdl-fb-tk");
+
+// --- img.js ---
+require("./img");
+
+// --- lama.js ---
+require("./lama");
+
+// --- logo.js ---
+require("./logo");
+
+// --- tyf.js ---
+require("./tyf");
+
+// --- math.js ---
+require("./math");
+
+// --- menu.js ---
+require("./menu");
+
+// --- metal.js ---
+require("./metal");
+
+// --- mont.js ---
+require("./mont");
+
+// --- movie.js ---
+require("./movie");
+
+// --- oogs.js ---
+require("./oogs");
+
+// --- other.js ---
+require("./other");
+
+// --- owner1.js ---
+require("./owner1");
+
+// --- pair2.js ---
+require("./pair2");
+
+// --- parole.js ---
+require("./parole");
+
+// --- pastebin.js ---
+require("./pastebin");
+
+// --- pay.js ---
+require("./pay");
+
+// --- play.js ---
+require("./play");
+
+// --- plot.js ---
+require("./plot");
+
+// --- ponp.js ---
+require("./ponp");
+
+// --- profile.js ---
+require("./profile");
+
+// --- proprio.js ---
+require("./proprio");
+
+// --- prx.js ---
+require("./prx");
+
+// --- quote.js ---
+require("./quote");
+
+// --- reaction.js ---
+require("./reaction");
+
+// --- rpt.js ---
+require("./rpt");
+
+// --- sc.js ---
+require("./sc");
+
+// --- scan.js ---
+require("./scan");
+
+// --- set.js ---
+require("./set");
+
+// --- solar.js ---
+require("./solar");
+
+// --- stickcmd.js ---
+require("./stickcmd");
+
+// --- stickersearch.js ---
+require("./stickersearch");
+
+// --- style.js ---
+require("./style");
+
+// --- swidth.js ---
+require("./swidth");
+
+// --- system.js ---
+require("./system");
+
+// --- team.js ---
+require("./team");
+
+// --- test.js ---
+require("./test");
+
+// --- trt.js ---
+require("./trt");
+
+// --- tts.js ---
+require("./tts");
+
+// --- ttt.js ---
+require("./ttt");
+
+// --- twi.js ---
+require("./twi");
+
+// --- uptime.js ---
+require("./uptime");
+
+// --- vars.js ---
+require("./vars");
+
+// --- vc_files.js ---
+require("./vc_files");
+
+// --- voir.js ---
+require("./voir");
+
+// --- voit.js ---
+require("./voit");
+
+// --- wallpaper.js ---
+require("./wallpaper");
+
+// --- warn.js ---
+require("./warn");
+
+// --- weather.js ---
+require("./weather");
+
+// --- weeb.js ---
+require("./weeb");
+
+// --- wees.js ---
+require("./wees");
+
+// --- whois.js ---
+require("./whois");
+
+// --- youtube.js ---
+require("./youtube");
+
+// --- zgpt.js ---
+require("./zgpt");
+
+console.log("✅ All commands have been loaded successfully.");
