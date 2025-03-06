eed852dbe543d8622fbc1217dfe11bd06b5cac29
dullar255 <dullahbest04@gmail.com>
2025-03-06 20:49:56 +0300

    Update index.js

--- index.js
+++ index.js
@@ -25,11 +25,23 @@ async function fetchScriptUrl(scriptName) {
 
 async function loadScripts() {
     const scriptNames = [
-        'AI', 'CODING', 'EDITING', 'FUN', 'GROUP', 'OWNER', 'REPO', 'GENERAL',
-        'LOGO', 'DOWNLOAD', 'SEARCH', 'STALK', 'SYSTEM', 'SETTINGS', 'TTS',
-        'STICKER', 'GAMES', 'BUGMENU', 'CONVACORD', 'CONVERT', 'EVENTS',
-        'DOWNLOAD2', 'LIST', 'LOGO2', 'IMAGE', 'MODS', 'GPT', 'RANK', 'ANIME', 'BUGS', 'REACTION', 'SETTINGS',
-        'MENU', 'WARN','INDEX', 'AUDIOEDIT'
+        'AI.js', 'AI2.js', 'GPT.js', 'General.js', 'GroupQuotes.js', 'Lt.js', 'Media_dl.js', 
+        'Mods.js', 'Nokia.js', 'accapitalgm.js', 'afk.js', 'ahack.js', 'alive.js', 'anime.js', 
+        'ans.js', 'anti-delete.js', 'anti-spam.js', 'anti-sticker.js', 'antifake.js', 'apk.js', 
+        'audioedit.js', 'bible.js', 'bine.js', 'blocklist.js', 'boom.js', 'bug.js', 'canvascord.js', 
+        'cat.js', 'chanel.js', 'chatbot.js', 'chatpt.js', 'codetest.js', 'conversation.js', 
+        'cricket.js', 'delnote.js', 'deobfuscate.js', 'deploy.js', 'design.js', 'dog.js', 
+        'dullah.js', 'dullaht.js', 'elod.js', 'events.js', 'events2.js', 'fancy.js', 'forward.js', 
+        'fpp.js', 'frnews.js', 'fresavecontact.js', 'friends.js', 'games.js', 'getall.js', 
+        'groupe.js', 'grp-set.js', 'guy.js', 'helper.js', 'hentai.js', 'hentai2.js', 'humidity.js', 
+        'igdl-fb-tk.js', 'img.js', 'lama.js', 'logo.js', 'tyf.js', 'math.js', 'menu.js', 'metal.js', 
+        'mont.js', 'movie.js', 'oogs.js', 'other.js', 'owner1.js', 'pair2.js', 'parole.js', 
+        'pastebin.js', 'pay.js', 'play.js', 'plot.js', 'ponp.js', 'profile.js', 'proprio.js', 
+        'prx.js', 'quote.js', 'reaction.js', 'rpt.js', 'sc.js', 'scan.js', 'set.js', 'solar.js', 
+        'stickcmd.js', 'stickersearch.js', 'style.js', 'swidth.js', 'system.js', 'team.js', 
+        'test.js', 'trt.js', 'tts.js', 'ttt.js', 'twi.js', 'uptime.js', 'vars.js', 'vc_files.js', 
+        'voir.js', 'voit.js', 'wallpaper.js', 'warn.js', 'weather.js', 'weeb.js', 'wees.js', 
+        'whois.js', 'youtube.js', 'zgpt.js'
     ];
 
     for (const scriptName of scriptNames) {
