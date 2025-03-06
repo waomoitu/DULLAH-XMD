4716a40a7367afa79fdbd2224d5e43c271e80730
dullar255 <dullahbest04@gmail.com>
2025-03-06 23:10:32 +0300

    Update and rename all_commands.js to xmd.js

---
+++ dullah/xmd.js
@@ -0,0 +1,15 @@
+"use strict";
+
+// All fetched commands combined here
+
+// --- AI.js ---
+const { someFunction } = require("../framework/zokou");
+module.exports = {};
+
+// --- GPT.js ---
+const axios = require("axios");
+module.exports = {};
+
+// --- General.js ---
+const fs = require("fs");
+module.exports = {};
