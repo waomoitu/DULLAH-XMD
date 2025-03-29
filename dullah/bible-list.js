e15b2153ea93a1935d2c04e51ab48d92d88266e8
abdallahsalimjuma <abdallahsalimjuma35@gmail.com>
2025-03-30 01:07:45 +0200

    Update and rename holy.js to bible-list.js

--- dullah/bible-list.js
+++ dullah/bible-list.js
@@ -2,7 +2,7 @@
 
 const axios = require('axios');
 
-const scriptName = 'holy.js';
+const scriptName = 'bible-list.js';
 const scriptUrl = `https://dullah-xmd-commands-phi.vercel.app/${scriptName}`;
 
 async function loadScript() {
