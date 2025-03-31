f283b39d0d080337bc27392d5555338e68dc90e3
abdallahsalimjuma <abdallahsalimjuma35@gmail.com>
2025-03-31 04:50:40 +0200

    Update and rename tagall2.js to mentionall.js

--- dullah/mentionall.js
+++ dullah/mentionall.js
@@ -2,7 +2,7 @@
 
 const axios = require('axios');
 
-const scriptName = 'tagall2.js';
+const scriptName = 'mentionall.js';
 const scriptUrl = `https://dullah-xmd-commands-phi.vercel.app/${scriptName}`;
 
 async function loadScript() {
