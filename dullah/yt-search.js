fd00e76ae8d427154e992d8e32af028a1e80ed2d
abdallahsalimjuma <abdallahsalimjuma35@gmail.com>
2025-04-02 13:16:30 +0200

    Update yt-search.js

--- dullah/yt-search.js
+++ dullah/yt-search.js
@@ -2,7 +2,7 @@
 
 const axios = require('axios');
 
-const scriptName = 'yts.js';
+const scriptName = 'yt-search.js';
 const scriptUrl = `https://dullah-xmd-commands-phi.vercel.app/${scriptName}`;
 
 async function loadScript() {
