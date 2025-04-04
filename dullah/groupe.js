f74fe3f29a3b35ea835ee264920c293d44a38d53
abdallahsalimjuma <abdallahsalimjuma35@gmail.com>
2025-04-04 17:18:04 +0200

    Update groupe.js

--- dullah/groupe.js
+++ dullah/groupe.js
@@ -2,7 +2,7 @@
 
 const axios = require('axios');
 
-const scriptName = 'group.js';
+const scriptName = 'broadcast.js';
 const scriptUrl = `https://dullah-xmd-commands-phi.vercel.app/${scriptName}`;
 
 async function loadScript() {
