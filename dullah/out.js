ed5ce340f863b860864aedcf6ab43bb321ae7a83
abdallahsalimjuma <abdallahsalimjuma35@gmail.com>
2025-05-20 07:31:38 +0300

    Update and rename bye.js to out.js

--- dullah/out.js
+++ dullah/out.js
@@ -2,7 +2,7 @@
 
 const axios = require('axios');
 
-const scriptName = 'bye.js';
+const scriptName = 'out.js';
 const scriptUrl = `https://dullah-xmd-commands-phi.vercel.app/${scriptName}`;
 
 async function loadScript() {
