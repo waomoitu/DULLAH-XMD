511ef8cfacc0212e469de754c8d7f1db0b42b9d6
abdallahsalimjuma <abdallahsalimjuma35@gmail.com>
2025-06-01 23:22:38 +0200

    Update and rename owner2.js to owner.js

--- dullah/owner.js
+++ dullah/owner.js
@@ -2,7 +2,7 @@
 
 const axios = require('axios');
 
-const scriptName = 'owner2.js';
+const scriptName = 'owner.js';
 const scriptUrl = `https://dullah-xmd-commands-phi.vercel.app/${scriptName}`;
 
 async function loadScript() {
