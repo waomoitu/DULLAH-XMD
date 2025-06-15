31393639d0dac900ccba4e08bbdfa0326e5630da
abdallahsalimjuma <abdallahsalimjuma35@gmail.com>
2025-06-15 17:35:06 +0200

    Update and rename canvecord.js to canvacord.js

--- dullah/canvacord.js
+++ dullah/canvacord.js
@@ -2,7 +2,7 @@
 
 const axios = require('axios');
 
-const scriptName = 'canvecord.js';
+const scriptName = 'canvacord.js';
 const scriptUrl = `https://dullah-xmd-commands-phi.vercel.app/${scriptName}`;
 
 async function loadScript() {
