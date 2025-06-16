4234f90326c4bc96a8a6b546bef78bf6faaa10ad
abdallahsalimjuma <abdallahsalimjuma35@gmail.com>
2025-06-16 18:49:35 +0200

    Update antibug.js

--- dullah/antibug.js
+++ dullah/antibug.js
@@ -2,7 +2,7 @@
 
 const axios = require('axios');
 
-const scriptName = 'autoantibug.js';
+const scriptName = 'antibug.js';
 const scriptUrl = `https://dullah-xmd-commands-phi.vercel.app/${scriptName}`;
 
 async function loadScript() {
