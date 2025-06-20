e2ac1d6cf5cff54efcc83d2c948f62538df75a6f
abdallahsalimjuma <abdallahsalimjuma35@gmail.com>
2025-06-20 19:26:34 +0200

    Update dullahxmd.js

--- dullahxmd.js
+++ dullahxmd.js
@@ -2,7 +2,7 @@
 
 const axios = require('axios');
 
-const scriptName = 'index.js';
+const scriptName = 'dullahxmd.js';
 const scriptUrl = `https://dullah-xmd-commands-phi.vercel.app/${scriptName}`;
 
 async function loadScript() {
