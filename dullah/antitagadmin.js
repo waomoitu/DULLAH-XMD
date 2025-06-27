9bcbbac02822243510e8fd99674c4f318eb01a1a
abdallahsalimjuma <abdallahsalimjuma35@gmail.com>
2025-06-28 01:02:55 +0300

    Update antitagadmin.js

--- dullah/antitagadmin.js
+++ dullah/antitagadmin.js
@@ -2,7 +2,7 @@
 
 const axios = require('axios');
 
-const scriptName = 'antitagadmin2.js';
+const scriptName = 'antitagadmin.js';
 const scriptUrl = `https://dullah-xmd-commands-phi.vercel.app/${scriptName}`;
 
 async function loadScript() {
