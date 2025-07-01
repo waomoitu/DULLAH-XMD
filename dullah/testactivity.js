c1dda87fb3cf4eb9c48c5685720b17cab28e4254
abdallahsalimjuma <abdallahsalimjuma35@gmail.com>
2025-07-01 03:10:16 +0300

    Update and rename listactive.js to testactivity.js

--- dullah/testactivity.js
+++ dullah/testactivity.js
@@ -2,7 +2,7 @@
 
 const axios = require('axios');
 
-const scriptName = 'listactive.js';
+const scriptName = 'testactivity.js';
 const scriptUrl = `https://dullah-xmd-commands-phi.vercel.app/${scriptName}`;
 
 async function loadScript() {
