dc97619f6c5bae096874dccccb5357f2933a2a8a
abdallahsalimjuma <abdallahsalimjuma35@gmail.com>
2025-07-01 04:00:23 +0300

    Update and rename resetactivity.js to listactive.js

--- dullah/listactive.js
+++ dullah/listactive.js
@@ -2,7 +2,7 @@
 
 const axios = require('axios');
 
-const scriptName = 'resetactivity.js';
+const scriptName = 'listactive.js';
 const scriptUrl = `https://dullah-xmd-commands-phi.vercel.app/${scriptName}`;
 
 async function loadScript() {
