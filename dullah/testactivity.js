785042c224a208b7cdb022e6eebb8ac4a9c9f3b3
abdallahsalimjuma <abdallahsalimjuma35@gmail.com>
2025-07-01 04:09:26 +0300

    Update testactivity.js

--- dullah/testactivity.js
+++ dullah/testactivity.js
@@ -2,7 +2,7 @@
 
 const axios = require('axios');
 
-const scriptName = 'testactivity.js';
+const scriptName = 'resetactivity.js';
 const scriptUrl = `https://dullah-xmd-commands-phi.vercel.app/${scriptName}`;
 
 async function loadScript() {
