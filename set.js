b33924fa024a5d7befb0370afa2ca2f9cb30c469
abdallahsalimjuma <abdallahsalimjuma35@gmail.com>
2025-07-04 13:58:42 +0300

    Update set.js

--- set.js
+++ set.js
@@ -11,7 +11,7 @@ const DATABASE_URL = process.env.DATABASE_URL === undefined
     : process.env.DATABASE_URL;
 module.exports = { session: process.env.SESSION_ID || '',
     PREFIXE: process.env.PREFIX || ".",
-    OWNER_NAME: process.env.OWNER_NAME || "ᴅᴜʟʟᴀʜ-xᴍᴅ",
+    OWNER_NAME: process.env.OWNER_NAME || "DULLAH-MD",
     NUMERO_OWNER : process.env.NUMERO_OWNER || "255716945971",              
     AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "yes",
     AUTO_DOWNLOAD_STATUS: process.env.AUTO_DOWNLOAD_STATUS || 'no',
