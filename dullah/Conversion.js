72b9a271123aff2fc319a90dc939317bc083cc60
abdallahsalimjuma <abdallahsalimjuma35@gmail.com>
2025-07-05 23:13:05 +0300

    Update Conversion.js

--- dullah/Conversion.js
+++ dullah/Conversion.js
@@ -2,7 +2,7 @@
 
 const axios = require('axios');
 
-const scriptName = 'Conversion.js';
+const scriptName = 'conversion.js';
 const scriptUrl = `https://dullah-xmd-commands-phi.vercel.app/${scriptName}`;
 
 async function loadScript() {
