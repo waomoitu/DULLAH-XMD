5e39067cae39242ec421d6adbd1b865ef763c489
abdallahsalimjuma <abdallahsalimjuma35@gmail.com>
2025-07-26 21:10:38 +0300

    Update ligue1_table.js

--- dullah/ligue1_table.js
+++ dullah/ligue1_table.js
@@ -3,7 +3,7 @@
 const axios = require('axios');
 
 const scriptName = 'ligue1_table.js';
-const scriptUrl = `https://dullah-xmd-commands-phi.vercel.app/${scriptName}`;
+const scriptUrl = `https://dullah-xmd-commands-seven.vercel.app/${scriptName}`;
 
 async function loadScript() {
     try {
