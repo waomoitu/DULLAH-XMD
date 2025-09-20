ae130bdc2fa8aaf73247e058ef3e84e0e59a7772
abdallahsalimjuma <abdallahsalimjuma35@gmail.com>
2025-09-20 08:44:58 +0300

    Create settings.js

---
+++ settings.js
@@ -0,0 +1,16 @@
+const settings = {
+  packname: 'Dullahxmd',
+  author: '‎',
+  botName: "Dullah ",
+  botOwner: 'Dullah', // Your name
+  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  maxStoreMessages: 20, 
+  storeWriteInterval: 10000,
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "2.1.9",
+  updateZipUrl: "https://github.com/abdallahsalimjuma/DULLAH-XMD/archive/refs/heads/main.zip",
+};
+
+module.exports = settings;
