1a94c332d37f1f07d9f3a5b57a75514087db880d
abdallahsalimjuma <abdallahsalimjuma35@gmail.com>
2025-09-20 08:47:40 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,7 +1,7 @@
 const settings = {
   packname: 'Dullahxmd',
   author: '‎',
-  botName: "Dullah ",
+  botName: ""ᴅᴜʟʟᴀʜ-xᴍᴅ v²",
   botOwner: 'Dullah', // Your name
   ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
