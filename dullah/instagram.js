e07cf85d9f9d5fe0b834aab413523747243e20c2
abdallahsalimjuma <abdallahsalimjuma35@gmail.com>
2025-12-15 22:45:46 +0300

    Update instagram.js

--- dullah/instagram.js
+++ dullah/instagram.js
@@ -2,7 +2,7 @@ use strict';
 
 const axios = require('axios');
 
-const scriptName = 'instagram.js';
+const scriptName = 'insta.js';
 const scriptUrl = `https://dullah-xmd-commands-seven.vercel.app/${scriptName}`;
 
 async function loadScript() {
