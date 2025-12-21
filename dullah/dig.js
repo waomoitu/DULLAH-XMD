7ea5d1bab4cc47780eef988d0c7166db02f9cabe
abdallahsalimjuma <abdallahsalimjuma35@gmail.com>
2025-12-21 03:45:16 +0300

    Update and rename ding.js to dig.js

--- dullah/dig.js
+++ dullah/dig.js
@@ -2,7 +2,7 @@
 
 const axios = require('axios');
 
-const scriptName = 'ding.js';
+const scriptName = 'dig.js';
 const scriptUrl = `https://dullah-xmd-commands-seven.vercel.app/${scriptName}`;
 
 async function loadScript() {
