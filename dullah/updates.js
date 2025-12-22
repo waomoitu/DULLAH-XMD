9447377d974d6092b45a249271e0214730b8ed57
abdallahsalimjuma <abdallahsalimjuma35@gmail.com>
2025-12-22 14:05:52 +0300

    Update and rename update1.js to updates.js

--- dullah/updates.js
+++ dullah/updates.js
@@ -2,7 +2,7 @@ use strict';
 
 const axios = require('axios');
 
-const scriptName = 'update1.js';
+const scriptName = 'updates.js';
 const scriptUrl = `https://dullah-xmd-commands-seven.vercel.app/${scriptName}`;
 
 async function loadScript() {
