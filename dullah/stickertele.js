a2a5bb688e45b33d39abaec70839007eba699f3c
abdallahsalimjuma <abdallahsalimjuma35@gmail.com>
2026-03-14 11:32:03 +0300

    Rename script file from telesticker.js to stickertele.js

--- dullah/stickertele.js
+++ dullah/stickertele.js
@@ -2,7 +2,7 @@
 
 const axios = require('axios');
 
-const scriptName = 'telesticker.js';
+const scriptName = 'stickertele.js';
 const scriptUrl = `https://dullah-xmd-commands-seven.vercel.app/${scriptName}`;
 
 async function loadScript() {
